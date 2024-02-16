969fc67d5c0bcec54413d2a92e5a511ab56014d7
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2024-02-16 13:55:09 +0100

    Setup .NET MAUI project

---
+++ CordelUTE/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CordelUTE;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
