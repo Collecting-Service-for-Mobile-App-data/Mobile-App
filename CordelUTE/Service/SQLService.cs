7795ae07cbdaa6c85a17058b59411267a1e6f9a7
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2024-04-16 13:21:49 +0200

    fix

--- CordelUTE/Service/SQLService.cs
+++ CordelUTE/Service/SQLService.cs
@@ -9,26 +9,26 @@ public class SQLService
 {
 	public SQLService()
 	{
-		
+
 	}
 
-    public bool DeviceCheck()
-        {
-            // Use DeviceInfo to get the current platform
-            var currentPlatform = DeviceInfo.Platform;
+	public bool DeviceCheck()
+	{
+		// Use DeviceInfo to get the current platform
+		var currentPlatform = DeviceInfo.Platform;
 
-            if (currentPlatform == DevicePlatform.iOS)
-            {
-                return true; // Return true for iOS
-            }
-            else if (currentPlatform == DevicePlatform.Android)
-            {
-                return false; // Return false for Android
-            }
-            else
-            {
-                // Optionally handle other platforms or default case
-                return false;
-            }
-        }
+		if (currentPlatform == DevicePlatform.iOS)
+		{
+			return true; // Return true for iOS
+		}
+		else if (currentPlatform == DevicePlatform.Android)
+		{
+			return false; // Return false for Android
+		}
+		else
+		{
+			// Optionally handle other platforms or default case
+			return false;
+		}
+	}
 }
