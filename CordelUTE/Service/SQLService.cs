7318f642657d0e6fdfae1fa5ea38c4ef286d74aa
Olek <oledole99@hotmail.no>
2024-04-16 12:47:48 +0200

    Added a SQLservice class

---
+++ CordelUTE/Service/SQLService.cs
@@ -0,0 +1,15 @@
+namespace MauiApp1;
+
+public class SQLService : ContentView
+{
+	public SQLService()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
